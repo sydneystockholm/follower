b9236ee0cc2ad088c5209cf30e15316b0dcc37ba
Chris O'Hara <cohara87@gmail.com>
2013-07-18 11:42:35 +1000

    Update docs

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-module.exports = require('./lib/follower');
+module.exports = {
+    Cache: require('./lib/cache').Cache
+  , Facebook: require('./lib/facebook').Facebook
+  , Google: require('./lib/google').Google
+  , Instagram: require('./lib/instagram').Instagram
+  , Pinterest: require('./lib/pinterest').Pinterest
+  , Twitter: require('./lib/twitter').Twitter
+  , Youtube: require('./lib/youtube').Youtube
+  , Bloglovin: require('./lib/bloglovin').Bloglovin
+  , request: require('./lib/request')
+};
 
